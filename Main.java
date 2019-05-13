9a16d981e41258ef5b853002fa0fb23624edf93a
Camille Brunet <camille.brunet.07@hotmail.fr>
2019-05-13 14:23:25 +0200

    DesignPattern

--- Main.java
+++ Main.java
@@ -3,10 +3,19 @@ public class Main {
     public static void main(String[] args) {
 
         Bank account = new Bank(1000);
+        
+        Transactions.withdraw(account, 100, new TransactionListener()
+            { 
+                public void onComplete(Bank account){
+                    System.out.printf("Your balance is: %d%n", account.getTotal());
+                }
+                
+            }); 
+        
+        
 
+        
         // TODO : create and use a TransactionListener interface with onComplete method
-        Transactions.withdraw(account, 100);
-
-        System.out.printf("Your balance is: %d%n", account.getTotal());
+        
     }
 }
